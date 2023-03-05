8c60d1925df0e28f1bb122233d3c7fc0d727e41f
wojtek <wojtej1174@gmail.com>
2023-03-05 11:28:41 +0100

    Grouping weight failes

---
+++ BeeWeight/src/Weight.cpp
@@ -0,0 +1,47 @@
+#include <HX711_ADC.h>
+#include <LiquidCrystal.h> //Dołączenie bilbioteki
+#include "Weight.h"
+LiquidCrystal lcd(2, 3, 4, 5, 6, 7); //Informacja o podłączeniu nowego wyświetlacza
+
+const int HX711_dout = 8;
+const int HX711_sck = 9;
+HX711_ADC LoadCell(HX711_dout, HX711_sck);
+
+void WeightConfigure(){
+
+    LoadCell.begin();
+  float calibrationValue;
+  calibrationValue = 22.39;
+
+  unsigned long stabilizingtime = 2000;
+ LoadCell.start(stabilizingtime);
+  if (LoadCell.getTareTimeoutFlag()) {
+    Serial.println("Timeout, check MCU>HX711 wiring and pin designations");
+    while (1);
+  } else {
+    LoadCell.setCalFactor(calibrationValue);  // set calibration value (float)
+    Serial.println("Startup is complete");
+  }
+}
+
+void WeightRun(){
+      static boolean newDataReady = 0;
+  const int serialPrintInterval = 5000;  //increase value to slow down serial print activity
+
+  // check for new data/start next conversion:
+  if (LoadCell.update()) newDataReady = true;
+
+  // get smoothed value from the dataset:
+  if (newDataReady) {
+    if (millis() > serialPrintInterval) {
+      float i = LoadCell.getData();
+      Serial.print(" ");
+      Serial.println(i - 367145 + 11);
+      newDataReady = 0;
+          //lcd.begin(16, 2); //Deklaracja typu
+  lcd.setCursor(0, 0); //Ustawienie kursora
+  lcd.print(i - 367145 + 11); //Wyświetlenie tekstu
+  delay(500);
+    }
+  }
+}
